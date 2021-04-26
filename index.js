542abeb806cb70e2a8f92739c93b62fd50ab1bcb
Aziza  iliasova <miacorporation25@gmail.com>
2021-04-26 18:23:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+let companyName = 'Scuber';
+it('is set as Scuber', function () {
+  expect(companyName).to.equal('Scuber');
+});// Code your solution in this file!
+
+
+
+let mostProfitableNeighborhood === 'Chelsea';
+function () {
+  expect(mostProfitableNeighborhood).to.equal('Chelsea');
+});// Code your solution in this file!
